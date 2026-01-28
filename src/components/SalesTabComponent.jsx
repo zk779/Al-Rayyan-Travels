bf870eddb51971cb0555c6418d80bc088fac9b35
zk779 <59329882+zk779@users.noreply.github.com>
2026-01-28 19:16:03 +0500

    Update SalesTabComponent.jsx

--- src/components/SalesTabComponent.jsx
+++ src/components/SalesTabComponent.jsx
@@ -615,7 +615,7 @@ useEffect(() => {
 									</div>
 
 									{/* Row 2: Financial Information */}
-									<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-3">
+									<div className={`grid grid-cols-2 ${isCredit ? "md:grid-cols-7" : "md:grid-cols-6"} gap-3`}>
 
 
 
