eb0bd32fe88a0398dd7a2990d74d4f019eb1b5e3
zk779 <59329882+zk779@users.noreply.github.com>
2026-07-20 22:31:09 +0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,6 +13,7 @@ const Layout = () => {
 const hideSidebarAndHeader =
   location.pathname === "/" ||
   location.pathname === "/login" ||
+  location.pathname === "/forbidden" ||
   location.pathname.startsWith("/invoice-print/");
 
   // Determine margin-left class based on sidebar state and current route
